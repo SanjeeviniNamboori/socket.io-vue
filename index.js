29e239590fd66d736a50e8247d89859392238af0
SanjeeviniNamboori <sanju.sanjeevini@gmail.com>
2017-10-13 14:35:11 +0530

    added vue

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var redis = require("redis");
-var sub=redis.createClient();
-var pub=redis.createClient();
+var sub = redis.createClient();
+var pub = redis.createClient();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+    console.log('Server listening at port %d', port);
 });
 
 // Routing
@@ -20,215 +20,252 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Chatroom
 
 var numUsers = 0;
-var users=[];
-var rooms=["default"];
-function checkRoom(room){
-var index= rooms.indexOf(room);
-console.log(index);
-if(index>-1)
-	return true;
-else return false;
+var users = [];
+var rooms = ["default"];
+function checkRoom(room) {
+    var index = rooms.indexOf(room);
+    console.log(index);
+    if (index > -1)
+        return true;
+    else return false;
 }
 
 
 
 sub.on("message", function (channel, data) {
-        data = JSON.parse(data);
-        console.log("Inside Redis_Sub: data from channel " + channel + ": " + (data.sendType));
-        if (parseInt("sendToSelf".localeCompare(data.sendType)) === 0) {
-             io.emit(data.sendTo, data.data);
-        }else if (parseInt("sendToAllConnectedClients".localeCompare(data.sendType)) === 0) {
-             io.sockets.emit(data.sendTo, data.data);
-        }else if (parseInt("sendToAllClientsInRoom".localeCompare(data.sendType)) === 0) {
-			console.log("emiting to "+channel +"and to method "+data.sendTo);
-            io.sockets.in(channel).emit(data.sendTo, data.data);
-        }
+    data = JSON.parse(data);
+    console.log("Inside Redis_Sub: data from channel " + channel + ": " + (data.sendType));
+    if (parseInt("sendToSelf".localeCompare(data.sendType)) === 0) {
+        io.emit(data.sendTo, data.data);
+    } else if (parseInt("sendToAllConnectedClients".localeCompare(data.sendType)) === 0) {
+        io.sockets.emit(data.sendTo, data.data);
+    } else if (parseInt("sendToAllClientsInRoom".localeCompare(data.sendType)) === 0) {
+        console.log("emiting to " + channel + "and to method " + data.sendTo);
+        io.sockets.in(channel).emit(data.sendTo, data.data);
+    }
 
-    });
+});
 
 
 
 
 io.on('connection', function (socket) {
-  var addedUser = false;
+    var addedUser = false;
 
-  io.emit('room added', {
+    io.emit('room added', {
 
-	  rooms:rooms
-	  });// when the client emits 'new message', this listens and executes
-  socket.on('new message', function (msg) {
-    // we tell the client to execute 'new message'
-
-	 var reply = JSON.stringify({
-                method: 'message',
-                sendType: 'sendToAllClientsInRoom',
-                data:{username: socket.username,
-				room:socket.room,message: msg},
-				sendTo:'new message'
-            });
-        pub.publish(socket.room,reply);
-
-	/*
-    socket.broadcast.to(socket.room).emit('new message', {
-      username: socket.username,
-      message: msg
-    });*/
-  });
-
-  socket.on('add room',function(room){
-	  if(checkRoom(room))return;
-	  rooms.push(room);
-	  //console.log("room added would be broadcasted"+rooms[0]);
-
-		var reply = JSON.stringify({
-                method: 'message',
-                sendType: 'sendToAllConnectedClients',
-                data:{username: socket.username,
-				rooms:rooms},
-				sendTo:'room added'
-            });
-        pub.publish(socket.room,reply);
-
-
-
-
-
-
-	/* io.emit('room added', {
-	  username: socket.username,
-	  rooms:rooms
-	  });*/
-	  /*socket.emit('login', {
-      numUsers: numUsers
+        rooms: rooms
     });
-	 */
 
-  });
+    // when the client emits 'new message', this listens and executes
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    console.log("in socket fun");
-    if (addedUser) return;
 
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-	socket.room="default";
-	socket.join(socket.room);
-	users.push(username);
-    addedUser = true;
-	//var room = io.adapter.rooms[socket.room];
-	sub.subscribe("default");
-    socket.emit('login', {
-      numUsers: numUsers,
-	  room:socket.room
+    socket.on('new message', function (msg) {
+        // we tell the client to execute 'new message'
+            console.log("in new message event" +  msg);
+        var reply = JSON.stringify({
+            method: 'message',
+            sendType: 'sendToAllClientsInRoom',
+            data: {
+                username: socket.username,
+                room: socket.room,
+                 message: msg
+            },
+            sendTo: 'new message'
+        });
+        pub.publish(socket.room, reply);
+
+        /*
+        socket.broadcast.to(socket.room).emit('new message', {
+          username: socket.username,
+          message: msg
+        });*/
     });
-	console.log(users);
-    // echo globally (all clients) that a person has connected
 
+    socket.on('add room', function (room) {
+        if (checkRoom(room)) return;
+        rooms.push(room);
+        //console.log("room added would be broadcasted"+rooms[0]);
 
-	 var reply = JSON.stringify({
-                method: 'message',
-                sendType: 'sendToAllClientsInRoom',
-                data:{username: socket.username,
-				room:socket.room},
-				sendTo:'user joined'
-            });
-        pub.publish(socket.room,reply);
-
-
-  /*  socket.broadcast.to(socket.room).emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers,
-	  room:socket.room
-    });*/
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.to(socket.room).emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.to(socket.room).emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    if (addedUser) {
-      --numUsers;
-		var index = users.indexOf(socket.username);
-		if (index > -1) {
-    users.splice(index, 1);
-}
-
-var reply = JSON.stringify({
-                method: 'message',
-                sendType: 'sendToAllClientsInRoom',
-                data:{username: socket.username,
-				room:socket.room},
-				sendTo:'user left'
-            });
-        pub.publish(socket.room,reply);
-
-	sub.quit();
-        pub.publish("chatting","User is disconnected :" + socket.id);
-
-      // echo globally that this client has left
-     /* socket.broadcast.to(socket.room).emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });*/
-	  socket.leave(socket.room);
-	  console.log(users);
+        var reply = JSON.stringify({
+            method: 'message',
+            sendType: 'sendToAllConnectedClients',
+            data: {
+                username: socket.username,
+                rooms: rooms
+            },
+            sendTo: 'room added'
+        });
+        pub.publish(socket.room, reply);
 
 
 
 
-	     }
-  });
 
 
-
-
-  socket.on('switchRoom', function(data){
-		// leave the current room (stored in session)
-		socket.leave(socket.room);
-		// join new room, received as function parameter
-		var newroom=data.newroom;
-		socket.join(newroom);
-
-
-		//socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-		// sent message to OLD room
-		//socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
-		socket.emit('clear');
-
-		socket.broadcast.to(socket.room).emit('user left', {
-        username: socket.username,
+        /* io.emit('room added', {
+          username: socket.username,
+          rooms:rooms
+          });*/
+        /*socket.emit('login', {
         numUsers: numUsers
       });
-		// update socket session room title
+       */
+
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+        //console.log("in socket fun");
+        //if (addedUser) return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        // console.log("server side" +  socket.username);
+        ++numUsers;
+        socket.room = "default";
+        socket.join(socket.room);
+        users.push(username);
+        addedUser = true;
+        //var room = io.adapter.rooms[socket.room];
+        sub.subscribe("default");
+        socket.emit('login', {
+            numUsers: numUsers,
+            room: socket.room
+        });
+        console.log(users);
+        // echo globally (all clients) that a person has connected
 
 
-var reply = JSON.stringify({
+        var reply = JSON.stringify({
+            method: 'message',
+            sendType: 'sendToAllClientsInRoom',
+            data: {
+                username: socket.username,
+                room: socket.room
+            },
+            sendTo: 'user joined'
+        });
+        pub.publish(socket.room, reply);
+
+
+        /*  socket.broadcast.to(socket.room).emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers,
+            room:socket.room
+          });*/
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+        socket.broadcast.to(socket.room).emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+        socket.broadcast.to(socket.room).emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    // when the user disconnects.. perform this
+  /*  socket.on('disconnect', function () {
+        if (addedUser) {
+            --numUsers;
+
+            var index = users.indexOf(socket.username);
+            if (index > -1) {
+                users.splice(index, 1);
+            }
+
+            var reply = JSON.stringify({
                 method: 'message',
                 sendType: 'sendToAllClientsInRoom',
-                data:{username: socket.username,
-				numUsers: numUsers,
-				room:socket.room},
-				sendTo:'user left'
+                data: {
+                    username: socket.username,
+                    room: socket.room
+                },
+                sendTo: 'user left'
             });
-        pub.publish(socket.room,reply);
+           pub.publish(socket.room, reply);
+
+           sub.quit();
+            pub.publish("chatting", "User is disconnected :" + socket.id);
+
+            // echo globally that this client has left
+            /* socket.broadcast.to(socket.room).emit('user left', {
+               username: socket.username,
+               numUsers: numUsers
+             });  */
+          /*  socket.leave(socket.room);
+            console.log(users);
 
 
-		socket.room = newroom;
-		sub.subscribe(newroom);
+
+
+        }
+    });
+*/
+        socket.on('disconnect' , function(){
+                if(addedUser){
+                    --numUsers;
+                    // socket.broadcast.to(socket.room).emit('user left', {
+                    //     username: socket.username,
+                    //     numUsers: numUsers
+                    //   }); 
+                    var reply = JSON.stringify({
+                        method: 'message',
+                        sendType: 'sendToAllClientsInRoom',
+                        data: {
+                            username: socket.username,
+                            room: socket.room
+                        },
+                        sendTo: 'user left'
+                    });
+                   pub.publish(socket.room, reply);
+        
+                }
+        }),
+
+
+    socket.on('switchRoom', function (data) {
+        // leave the current room (stored in session)
+        console.log("room left" + socket.room)
+        socket.leave(socket.room);
+        // join new room, received as function parameter
+        var newroom = data.newroom;
+        socket.join(newroom);
+
+
+        //socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+        // sent message to OLD room
+        //socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+        socket.emit('clear');
+
+        socket.broadcast.to(socket.room).emit('user left', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+        // update socket session room title
+
+
+        var reply = JSON.stringify({
+            method: 'message',
+            sendType: 'sendToAllClientsInRoom',
+            data: {
+                username: socket.username,
+                numUsers: numUsers,
+                room: socket.room
+            },
+            sendTo: 'user left'
+        });
+        pub.publish(socket.room, reply);
+
+
+        socket.room = newroom;
+        sub.subscribe(newroom);
 
 
 		/*
@@ -238,22 +275,24 @@ var reply = JSON.stringify({
 	  room:socket.room
     });
 	*/
-	//socket.emit('updaterooms', rooms, newroom);
-	 reply = JSON.stringify({
-                method: 'message',
-                sendType: 'sendToAllClientsInRoom',
-                data:{username: socket.username,
-				room:socket.room},
-				sendTo:'user joined'
-            });
-        pub.publish(socket.room,reply);
+        //socket.emit('updaterooms', rooms, newroom);
+        reply = JSON.stringify({
+            method: 'message',
+            sendType: 'sendToAllClientsInRoom',
+            data: {
+                username: socket.username,
+                room: socket.room
+            },
+            sendTo: 'user joined'
+        });
+        pub.publish(socket.room, reply);
 
 
 
 
-	});
+    });
 
-  sub.on("subscribe", function(channel, count) {
+    sub.on("subscribe", function (channel, count) {
         console.log("Subscribed to " + channel + ". Now subscribed to " + count + " channel(s).");
     });
 
--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
   var COLORS = [
@@ -15,36 +15,36 @@ $(function() {
 
   var $rooms = $('.rooms'); // Rooms area
   var $inputRoom = $('.inputRoom'); // Input Room input box
-  
+
   var $loginPage = $('.login.page'); // The login page
   var $chatPage = $('.chat.page'); // The chatroom page
-  var $roomPage=$('.room.page');//The room page
+  var $roomPage = $('.room.page');//The room page
   // Prompt for setting a username
-  
-  var $roomName =$('.roomData');
+
+  var $roomName = $('.roomData');
   var username;
-  var room=false;
+  var room = false;
   var connected = false;
   var typing = false;
   var lastTypingTime;
   var $currentInput = $usernameInput.focus();
 
   var socket = io();
-  
-  
-  function addParticipantsMessage (data) {
+
+
+  function addParticipantsMessage(data) {
     var message = '';
     if (data.numUsers === 1) {
       message += "there's 1 participant";
     } else {
       message += "there are " + data.numUsers + " participants";
     }
-	message +=" "+data.room;
+    message += " " + data.room;
     log(message);
   }
 
   // Sets the client's username
-  function setUsername () {
+  function setUsername() {
     username = cleanInput($usernameInput.val().trim());
 
     // If the username is valid
@@ -60,7 +60,7 @@ $(function() {
   }
 
   // Sends a chat message
-  function sendMessage () {
+  function sendMessage() {
     var message = $inputMessage.val();
     // Prevent markup from being injected into the message
     message = cleanInput(message);
@@ -77,13 +77,13 @@ $(function() {
   }
 
   // Log a message
-  function log (message, options) {
+  function log(message, options) {
     var $el = $('<li>').addClass('log').text(message);
     addMessageElement($el, options);
   }
 
   // Adds the visual chat message to the message list
-  function addChatMessage (data, options) {
+  function addChatMessage(data, options) {
     // Don't fade the message in if there is an 'X was typing'
     var $typingMessages = getTypingMessages(data);
     options = options || {};
@@ -105,18 +105,18 @@ $(function() {
       .append($usernameDiv, $messageBodyDiv);
 
     addMessageElement($messageDiv, options);
-	console.log("added chat message");
+    console.log("added chat message");
   }
 
   // Adds the visual chat typing message
-  function addChatTyping (data) {
+  function addChatTyping(data) {
     data.typing = true;
     data.message = 'is typing';
     addChatMessage(data);
   }
 
   // Removes the visual chat typing message
-  function removeChatTyping (data) {
+  function removeChatTyping(data) {
     getTypingMessages(data).fadeOut(function () {
       $(this).remove();
     });
@@ -127,7 +127,7 @@ $(function() {
   // options.fade - If the element should fade-in (default = true)
   // options.prepend - If the element should prepend
   //   all other messages (default = false)
-  function addMessageElement (el, options) {
+  function addMessageElement(el, options) {
     var $el = $(el);
 
     // Setup default options
@@ -154,12 +154,12 @@ $(function() {
   }
 
   // Prevents input from having injected markup
-  function cleanInput (input) {
+  function cleanInput(input) {
     return $('<div/>').text(input).html();
   }
 
   // Updates the typing event
-  function updateTyping () {
+  function updateTyping() {
     if (connected) {
       if (!typing) {
         typing = true;
@@ -179,18 +179,18 @@ $(function() {
   }
 
   // Gets the 'X is typing' messages of a user
-  function getTypingMessages (data) {
+  function getTypingMessages(data) {
     return $('.typing.message').filter(function (i) {
       return $(this).data('username') === data.username;
     });
   }
 
   // Gets the color of a username through our hash function
-  function getUsernameColor (username) {
+  function getUsernameColor(username) {
     // Compute hash code
     var hash = 7;
     for (var i = 0; i < username.length; i++) {
-       hash = username.charCodeAt(i) + (hash << 5) - hash;
+      hash = username.charCodeAt(i) + (hash << 5) - hash;
     }
     // Calculate color
     var index = Math.abs(hash % COLORS.length);
@@ -207,11 +207,11 @@ $(function() {
     // When the client hits ENTER on their keyboard
     if (event.which === 13) {
       console.log(room);
-	  if(room){
-	  room=false;
-	  addRoom();
-	  }
-	  else if (username) {
+      if (room) {
+        room = false;
+        addRoom();
+      }
+      else if (username) {
         sendMessage();
         socket.emit('stop typing');
         typing = false;
@@ -221,14 +221,14 @@ $(function() {
     }
   });
 
-  $inputMessage.on('input', function() {
+  $inputMessage.on('input', function () {
     updateTyping();
   });
 
-  
-  
-  
-  
+
+
+
+
   // Click events
 
   // Focus input when clicking anywhere on login page
@@ -238,28 +238,28 @@ $(function() {
 
   // Focus input when clicking on the message input's border
   $inputMessage.click(function () {
-    room=false;
-	$inputMessage.focus();
-	$currentInput = $inputMessage.focus();
-	
+    room = false;
+    $inputMessage.focus();
+    $currentInput = $inputMessage.focus();
+
   });
   $inputRoom.click(function () {
-    room=true;
-	$inputRoom.focus();
-	$currentInput = $inputRoom.focus();
-	
-  });
-  
-  
-  
-  $('.roomData').click( function() {
-	  //var room=this.id;
-	  console.log("click data");
-    	//socket.emit('switchRoom', room);
-	
+    room = true;
+    $inputRoom.focus();
+    $currentInput = $inputRoom.focus();
+
   });
 
-  
+
+
+  $('.roomData').click(function () {
+    //var room=this.id;
+    console.log("click data");
+    //socket.emit('switchRoom', room);
+
+  });
+
+
 
   // Socket events
 
@@ -271,20 +271,20 @@ $(function() {
     log(message, {
       prepend: true
     });
-   // addParticipantsMessage(data);
+    // addParticipantsMessage(data);
   });
 
   // Whenever the server emits 'new message', update the chat body
   socket.on('new message', function (data) {
-	 
+
     addChatMessage(data);
   });
 
   // Whenever the server emits 'user joined', log it in the chat body
   socket.on('user joined', function (data) {
-	  console.log(data);
-	  console.log("   user joined");
-    log(data.username + ' joined '+data.room);
+    console.log(data);
+    console.log("   user joined");
+    log(data.username + ' joined ' + data.room);
     //addParticipantsMessage(data);
   });
 
@@ -319,52 +319,52 @@ $(function() {
   socket.on('reconnect_error', function () {
     log('attempt to reconnect has failed');
   });
-  
+
   socket.on('clear', function () {
     $messages.empty();
   });
-  
-  
-  
+
+
+
   // Whenever the server emits 'room added', log it in the chat body
   socket.on('room added', function (data) {
-    
-	//console.log("room added");
-	$rooms.empty();
-	console.log(data);
-	for(var i=0;i<data.rooms.length;i++){
-	$rooms.append('<li class="roomData"  onclick="gotoRoom(event)">'+data.rooms[i]+'</a></li>');
-	//console.log(data.rooms[i]);
-	}
-	
-	
-	//chatLog(data.rooms + ' joined');
+
+    //console.log("room added");
+    $rooms.empty();
+    console.log(data);
+    for (var i = 0; i < data.rooms.length; i++) {
+      $rooms.append('<li class="roomData"  onclick="gotoRoom(event)">' + data.rooms[i] + '</a></li>');
+      //console.log(data.rooms[i]);
+    }
+
+
+    //chatLog(data.rooms + ' joined');
     //addParticipantsMessage(data);
   });
 
-  
-  
-  
-  
-  window.gotoRoom=function(event){
-
-//console.log(event);
-  //event.preventDefault();
-  var gotoRoom =   event.currentTarget.innerHTML;
-  console.log(event.currentTarget.innerHTML);
-  socket.emit('switchRoom',{newroom: gotoRoom});
-  //socket.emit('join',gotoRoom );
-//var t = document.getElementById('gotoRoom').value;
-//  console.log("hey" +t );
 
 
-}
-  
-  
-  
-  function addRoom () {
+
+
+  window.gotoRoom = function (event) {
+
+    //console.log(event);
+    //event.preventDefault();
+    var gotoRoom = event.currentTarget.innerHTML;
+    console.log(event.currentTarget.innerHTML);
+    socket.emit('switchRoom', { newroom: gotoRoom });
+    //socket.emit('join',gotoRoom );
+    //var t = document.getElementById('gotoRoom').value;
+    //  console.log("hey" +t );
+
+
+  }
+
+
+
+  function addRoom() {
     var roomName = $inputRoom.val();
-	console.log(roomName);
+    console.log(roomName);
     // Prevent markup from being injected into the message
     roomName = cleanInput(roomName);
     // if there is a non-empty message and a socket connection
@@ -378,15 +378,15 @@ $(function() {
       socket.emit('add room', roomName);
     }
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
 });
 /*
 var socket = io();
